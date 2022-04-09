f8972cb188ca5d0217be08e3b4deb181a38fb38d
陳君豪 <justin.uob@gmail.com>
2022-04-09 15:19:09 +0800

    almost finished

--- src/main/java/com/crypto/calculator/ChangeMoneyService.java
+++ src/main/java/com/crypto/calculator/ChangeMoneyService.java
@@ -17,14 +17,11 @@ import com.google.gson.JsonObject;
 /**
  * This service calculate daily based VWAP(Volume Weighted Average Price)
  */
-public class VWAPService {
+public class ChangeMoneyService {
 
-    public BigDecimal calculate(int numOfDays){
-        String url = "https://api.crypto.com/v2/public/get-candlestick?instrument_name=CRO_USDT&timeframe=1D";
+    public BigDecimal calculate(int numOfDays, String url){
         InputStream is = null;
-
         BigDecimal vwap = BigDecimal.ZERO;
-
         try{
             is = new URL(url).openStream();
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
@@ -53,7 +50,10 @@ public class VWAPService {
 
                 // calculate tpv
                 double typicalPrice = (data.getH()+data.getL()+data.getC()) / 3;
-                BigDecimal tp = new BigDecimal(typicalPrice).setScale(4, RoundingMode.HALF_UP);
+
+                // new BigDecimal(Double.toString(c)) / BigDecimal.valueOf(typicalPrice) either way is fine
+                // Notes: do not do -> BigDecimal(double val) The results of this constructor can be somewhat unpredictable.
+                BigDecimal tp = new BigDecimal(Double.toString(typicalPrice)).setScale(4, RoundingMode.HALF_UP);
                 BigDecimal tpv = tp.multiply(data.getV()).setScale(4, RoundingMode.HALF_UP);
                 data.setTpv(tpv);
 
@@ -66,7 +66,6 @@ public class VWAPService {
             // Calculate VWAP with your information: [cumulative TPV ÷ cumulative volume]. This will provide a volume-weighted average price for each period
             vwap = cumulativeTpv.divide(cumulativeVol, 4, RoundingMode.HALF_UP);
             // e.g. vwap = 0.3812 = 0.3812 USD for 1 CRO (exchange rate)
-            System.out.println(vwap);
 
         }catch(IOException e){
             e.printStackTrace();
@@ -81,4 +80,37 @@ public class VWAPService {
         return vwap;
     }
 
+
+    public BigDecimal exchangeCROs(double usd, BigDecimal vwap) {
+        BigDecimal dollars = new BigDecimal(Double.toString(usd)).setScale(4, RoundingMode.HALF_UP);
+        // https://crypto.com/price/crypto-com-coin -> CRO: two decimal places
+        return dollars.divide(vwap,2,RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal cashOutUSD(BigDecimal cros, double exchangeRate) {
+        BigDecimal exchangeRateOfTheDay = new BigDecimal(Double.toString(exchangeRate)).setScale(4, RoundingMode.HALF_UP);
+        return cros.multiply(exchangeRateOfTheDay).setScale(4,RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal cappedUSD(int year, BigDecimal usd) throws Exception {
+        double cap = 0d;
+        switch (year){
+            case 1:
+                cap = 0.1;
+                break;
+            case 2:
+                cap = 0.2;
+                break;
+            case 3:
+                cap = 0.3;
+                break;
+            case 4:
+                cap = 0.4;
+                break;
+            default:
+                throw new Exception("year out of bound!");
+        }
+        BigDecimal capRate = new BigDecimal(Double.toString(cap)).setScale(4, RoundingMode.HALF_UP);
+        return usd.multiply(capRate).setScale(4,RoundingMode.HALF_UP);
+    }
 }
--- src/main/java/com/crypto/calculator/Driver.java
+++ src/main/java/com/crypto/calculator/Driver.java
@@ -1,16 +1,37 @@
 package com.crypto.calculator;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Driver {
-    public static void main(String[] args) {
-        //  This service calculate daily based VWAP(Volume Weighted Average Price)
-        VWAPService service = new VWAPService();
-        int numberOfDays = 365;
-        BigDecimal vwap = service.calculate(numberOfDays);
-        System.out.println(vwap);
+    /*
+    Goal: design a simple program to help Alice to calculate her actual rewards in USD.
+        step 1: calculate the daily based vwap in the first 365 days since the employee joined the company.
+        step 2: identified how many CROs are to be granted (according to vwap)
+        step 3: calculate how much USD is to be granted (depends on the CRO price of the day)
+        step 4: calculate how much USD is to be granted with each anniversary's cap
+     */
+    public static void main(String[] args) throws Exception {
 
+        ChangeMoneyService service = new ChangeMoneyService(); //  This service calculate daily based VWAP(Volume Weighted Average Price)
+        int numberOfDays = 365; // today is her 1st year anniversary, so trace back 365 days
+        // VWAP can be calculated with the public candlestick API
+        String apiUrl = "https://api.crypto.com/v2/public/get-candlestick?instrument_name=CRO_USDT&timeframe=1D";
+        BigDecimal vwap = service.calculate(numberOfDays, apiUrl);
+        System.out.println("vwap: "+vwap);
 
+        double X = 100; // Alice Wong's base yearly package of X US dollars
+        BigDecimal CROs = service.exchangeCROs(X, vwap);
+        System.out.println("CROs: "+CROs);
 
+        // 1 CRO = 0.4377 USD -> can be found in https://crypto.com/price/crypto-com-coin
+        double exchangeRateOfTheDay = 0.4377d; // an arbitrary number
+        BigDecimal usd = service.cashOutUSD(CROs, exchangeRateOfTheDay);
+        System.out.println("uncapped USD: " + usd);
+
+        // today is her 1st year anniversary -> 10% cap
+        int year = 1;
+        BigDecimal cappedUSD = service.cappedUSD(year,usd);
+        System.out.println("capped USD: " + cappedUSD +"(Alice Wong's actual rewards in USD)");
     }
 }
